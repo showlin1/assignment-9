c6017085d297ce4f96cf1803c935a3f5d43582c6
unknown <showlinakter77@gmail.com>
2024-06-02 16:40:21 +0600

    register page and login page completed

---
+++ src/Providers/AuthProvider.jsx
@@ -0,0 +1,79 @@
+import { createUserWithEmailAndPassword, GoogleAuthProvider, GithubAuthProvider, getAuth, signInWithPopup, onAuthStateChanged, signOut, signInWithEmailAndPassword } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.config";
+
+
+
+export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
+
+const googleProvider = new GoogleAuthProvider();
+
+const githubProvider = new GithubAuthProvider();
+
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    const logOut = () =>{
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log('user in the auth state changed', currentUser);
+            setUser(currentUser);
+        });
+        return () => {
+            unSubscribe();
+        }
+    }, [])
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signIn = (email,password) =>{
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+
+    const handleGoogleSignIn = () => {
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                console.log(result.user)
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
+    const handleGithubSignIn = () => {
+        signInWithPopup(auth, githubProvider)
+            .then(result => {
+                console.log(result.user)
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
+
+
+
+
+    const authInfo = {
+        user,
+        createUser,
+        handleGoogleSignIn,
+        handleGithubSignIn,
+        signIn,
+        logOut
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
